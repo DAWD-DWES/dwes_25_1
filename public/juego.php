4d8bcc1d081e2b7f28348f67f40c552a419cf710
profesor <profesor@dawdistancia.net>
2025-03-14 19:33:50 +0100

    fix

--- public/juego.php
+++ public/juego.php
@@ -79,10 +79,10 @@ if (isset($_SESSION['usuario'])) {
         $rutaFichero = $_ENV['RUTA_ALMACEN_PALABRAS'];
         $almacenPalabras = new AlmacenPalabrasFichero($rutaFichero);
         $partida = new Partida($almacenPalabras, MAX_NUM_ERRORES);
-        $_SESSION['partida'] = $partida;
         $partida->setIdUsuario($usuario->getId());
         $partidaId = $partidaDAO->crea($partida);
         $partida->setId($partidaId);
+        $_SESSION['partida'] = $partida;
 // Invoco la vista del juego para empezar a jugar
         echo $blade->run("juego", compact('usuario', 'partida'));
         // Si no si se resuelve la partida con una palabra
